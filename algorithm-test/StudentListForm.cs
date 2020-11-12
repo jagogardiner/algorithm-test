1aa4d0fd94a2a4d717e40ba51ba7b12739317e88
nysascape <jago@nysascape.tech>
2020-11-12 20:07:06 +0000

    not sure what i changed again

--- algorithm-test/StudentListForm.cs
+++ algorithm-test/StudentListForm.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace algorithm_test
