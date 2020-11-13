36cb02bd82c16c006c5eb85a7c2876409998b4dc
nysascape <jago@nysascape.tech>
2020-11-13 13:27:33 +0000

    Remove ignored files

--- dbo.Students.data.sql
+++
@@ -1,9 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Students] ON
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (1, N'Jago', N'Gardiner', 315418, 1)
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (2, N'Hannah', N'Emerton', 315417, 1)
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (3, N'Abby', N'George', 316348, 1)
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (4, N'Harvey', N'Hewitt', 315739, 1)
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (5, N'Ella', N'Kilpatrick', 317283, 2)
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (6, N'Conan', N'Millen', 315841, 1)
-INSERT INTO [dbo].[Students] ([StudentId], [StudentFirstName], [StudentLastName], [StudentCollegeID], [AcademicYearID]) VALUES (7, N'Megan', N'Thomas', 316281, 1)
-SET IDENTITY_INSERT [dbo].[Students] OFF
